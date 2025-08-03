e6c0f84e728bb88921adbc7699bae1c4d61d1d68
Adam Wheatley <wheatley.adam@me.com>
2025-08-02 20:44:19 -0500

    use BrowserRouter for React routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,18 @@
 import { useState } from 'react'
 import './App.css'
 import PostsList from './components/PostsList';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
-  return <>
+  return (
+  <Router>
     <div className='app'>
       <h1>ROR React Project 1</h1>
       <p>Find this application in client/src/App.jsx</p>
       <PostsList />
     </div>
-  </>
+  </Router>
+  );
 }
 
 export default App
